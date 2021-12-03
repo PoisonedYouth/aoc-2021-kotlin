85c80cf64f919da03675b617fdc87f84c0803656
Matthias Schenk | sevDesk <matthias.schenk@sevdesk.de>
2021-12-03 12:12:01 +0100

    Optimized solution

--- src/Day03.kt
+++ src/Day03.kt
@@ -14,9 +14,16 @@ private fun solveProblemOne(lines: List<String>) {
     var gammaRate = ""
     var epsilonRate = ""
     for (index in 0 until lines.first().length) {
-        val groups = lines.map { it[index] }.groupBy { it }
-        gammaRate += if (groups[ZERO]!!.size > groups[ONE]!!.size) ZERO else ONE
-        epsilonRate += if (groups[ZERO]!!.size > groups[ONE]!!.size) ONE else ZERO
+        lines.map { it[index] }.groupBy { it }.let {
+            if (it[ZERO]!!.size > it[ONE]!!.size) {
+                gammaRate += ZERO
+                epsilonRate += ONE
+            } else {
+                gammaRate += ONE
+                epsilonRate += ZERO
+            }
+        }
+
     }
     println(gammaRate.toInt(2) * epsilonRate.toInt(2))
 }
@@ -27,19 +34,12 @@ private fun solveProblemTwo(lines: List<String>) {
     for (index in 0 until lines.first().length) {
         if (oxygenRatingList.size > 1) {
             val groups = oxygenRatingList.map { it[index] }.groupBy { it }
-            oxygenRatingList = if (groups[ZERO]!!.size > groups[ONE]!!.size) {
-                oxygenRatingList.filter { it[index] == ZERO }
-            } else {
-                oxygenRatingList.filter { it[index] == ONE }
-            }
+            oxygenRatingList = oxygenRatingList.filter { it[index] == if (groups[ZERO]!!.size > groups[ONE]!!.size) ZERO else ONE }
         }
         if (scrubberRatingList.size > 1) {
             val groups = scrubberRatingList.map { it[index] }.groupBy { it }
-            scrubberRatingList = if (groups[ZERO]!!.size > groups[ONE]!!.size) {
-                scrubberRatingList.filter { it[index] == ONE }
-            } else {
-                scrubberRatingList.filter { it[index] == ZERO }
-            }
+            scrubberRatingList = scrubberRatingList.filter { it[index] == if (groups[ZERO]!!.size > groups[ONE]!!.size) ONE else ZERO }
+
         }
     }
     println(oxygenRatingList[0].toInt(2) * scrubberRatingList[0].toInt(2))
